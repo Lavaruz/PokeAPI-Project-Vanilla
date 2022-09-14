96111061a1b721fe769f52b9da36cc0c3e7bf0cd
Assami Muzaki <muzakiassami@gmail.com>
2022-09-14 22:35:40 +0700

    first fetch

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+// const body = document.querySelector('body')
+
 async function fetchAPI(){
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon-form/1/')
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/1/')
+    if(response.ok){
+        console.log('SUCCESS FETCH');
+    }
     return response.json()
 }
 
-fetchAPI().then(data => console.log(data.name))
+
+const pokemonName = fetchAPI().then(data => {
+    const newH3 = document.createElement('h3')
+    newH3.innerHTML = data.name
+    const newImg = document.createElement('img')
+    newImg.src = data.sprites.front_shiny
+
+    document.body.appendChild(newH3)
+    document.body.appendChild(newImg)
+})
+// const pokemonFront = fetchAPI().then(data => data.sprites.front_shiny)
+
+// console.log(pokemonName);
+
